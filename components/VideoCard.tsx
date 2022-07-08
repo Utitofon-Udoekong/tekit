61c8a5f6613a7f0f21bfa231fdc2f449ea9e0590
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-07-08 22:11:14 +0100

    ok

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,6 +1,12 @@
-import React, {use} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import { NextPage } from "next";
 import { Video } from "../types";
+import Image from "next/image"
+import Link from 'next/Link'
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+import { BsPlay, BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go';
+
 
 interface IProps {
   post: Video;
@@ -8,10 +14,27 @@ interface IProps {
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
   return (
-    <div>
-        VideoCard
+    <div className="flex flex-col border-b-2 border-gray-200 pb-6">
+        <div>
+            <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+                <div className="md:w-16md:h-16 w-10 h-10">
+                    <Link href="/">
+                        <>
+                            <Image 
+                            width={62}
+                            height={62}
+                            className="rounded-full"
+                            src={post.postedBy.image}
+                            alt="Profile Photo"
+                            layout="responsive"
+                            />
+                        </>
+                    </Link>
+                </div>
+            </div>
+        </div>
     </div>
   );
 };
 
-export default Video
+export default VideoCard;
