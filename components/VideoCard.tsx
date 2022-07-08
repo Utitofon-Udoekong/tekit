6034d61787501e9d75ec01376ce234cf0b570185
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-07-08 22:17:58 +0100

    ok

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,12 +1,11 @@
-import React, {useState, useEffect, useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { NextPage } from "next";
 import { Video } from "../types";
-import Image from "next/image"
-import Link from 'next/Link'
-import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
-import { BsPlay, BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
-import { GoVerified } from 'react-icons/go';
-
+import Image from "next/image";
+import Link from "next/Link";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import { BsPlay, BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
 
 interface IProps {
   post: Video;
@@ -15,24 +14,37 @@ interface IProps {
 const VideoCard: NextPage<IProps> = ({ post }) => {
   return (
     <div className="flex flex-col border-b-2 border-gray-200 pb-6">
-        <div>
-            <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
-                <div className="md:w-16md:h-16 w-10 h-10">
-                    <Link href="/">
-                        <>
-                            <Image 
-                            width={62}
-                            height={62}
-                            className="rounded-full"
-                            src={post.postedBy.image}
-                            alt="Profile Photo"
-                            layout="responsive"
-                            />
-                        </>
-                    </Link>
-                </div>
-            </div>
+      <div>
+        <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+          <div className="md:w-16 md:h-16 w-10 h-10">
+            <Link href="/">
+              <>
+                <Image
+                  width={62}
+                  height={62}
+                  className="rounded-full"
+                  src={post.postedBy.image}
+                  alt="Profile Photo"
+                  layout="responsive"
+                />
+              </>
+            </Link>
+          </div>
+          <div>
+            <Link href="/">
+              <div>
+                <p>
+                  {post.postedBy.userName}{" "}
+                  <GoVerified className="text-blue-400 text-md" />
+                </p>
+                <p>
+                    {post.postedBy.userName}
+                </p>
+              </div>
+            </Link>
+          </div>
         </div>
+      </div>
     </div>
   );
 };
