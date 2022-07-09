5825854137e2fd776df073e91f3485b66a6a8b2b
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-07-09 22:15:38 +0100

    ok

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,9 +8,10 @@ import {BiSearch} from 'react-icons/bi'
 import {IoMdAdd} from 'react-icons/io'
 import Logo from '../utils/tiktik-logo.png'
 import { createOrGetUser } from '../utils'
+import useAuthStore from '../store/authStore'
 
 const Navbar = () => {
-  const user = false
+  const {userProfile, addUser} = useAuthStore()
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
         <Link href="/">
@@ -28,11 +29,33 @@ const Navbar = () => {
         </div>
         <div>
           {
-            user ? (
-              <div>Loggedin</div>
+            userProfile ? (
+              <div className="gap-5 flex md:gap-10">
+                <Link href="/">
+                  <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+                    <IoMdAdd className='text-xl' /> {` `}
+                    <span className='hidden md:block'>Upload</span>
+                  </button>
+                </Link>
+
+                {userProfile.image && (
+                  <Link href="/">
+                  <>
+                    <Image
+                      width={62}
+                      height={62}
+                      className="rounded-full"
+                      src={userProfile.image}
+                      alt="Profile Photo"
+                      layout="responsive"
+                    />
+                  </>
+                </Link>
+                )}
+              </div>
             ) : (
               <GoogleLogin
-              onSuccess={(response) => createOrGetUser(response)}
+              onSuccess={(response) => createOrGetUser(response, addUser)}
               onError={() => console.log("error")}/>
             )
           }
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["thumbs.dreamstime.com"]
+    domains: ["thumbs.dreamstime.com", "lh3.googleusercontent.com"]
   }
 }
 
