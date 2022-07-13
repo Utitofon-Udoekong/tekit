3f9a332585e9dfd964fad273bd376919cd040e63
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-07-13 23:24:20 +0100

    ok

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -8,15 +8,43 @@ import {BsFillPlayFill} from 'react-icons/bs'
 import {HiVolumeUp, HiVolumeOff} from 'react-icons/hi'
 import axios from 'axios'
 import { BASE_URL } from '../../utils'
+import { Video } from '../../types'
 
-const Detail = () => {
+interface IProps{
+  postDetails: Video
+}
+
+const Detail = ({postDetails}: IProps) => {
+  const [post, setPost] = useState(postDetails)
+  use
+  const videoRef = useRef(null)
+  if(!post) return null
   return (
-    <div>Detail</div>
+    <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap '>
+      <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center" >
+        <div className="absolute top-6 left-2 flex gap-6 z-50 lg:left-6">
+          <p><MdOutlineCancel className="text-white text-[35px]"/></p>
+        </div>
+        <div className="relative">
+          <div className="lg:h-screen h-[60vh]">
+            <video ref={videoRef} loop onClick={() => {}} src={post.video.asset.url} className="h-full cursor-pointer"></video>
+          </div>
+          <div>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export const getServerSideProps = async ({params: {id}}) => {
+export const getServerSideProps = async ({params: {id}}: {params: {id: string}}) => {
   const {data} = await axios.get(`${BASE_URL}/api/post/${id}`)
+  return {
+    props: {
+      postDetails: data
+    }
+  }
 }
 
 export default Detail
